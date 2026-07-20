5bed49a9c605d711051adedbc595f27f93574498
NurinDamiaa <nurindamia200527@gmail.com>
2026-07-20 16:01:02 +0800

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once __DIR__ . '/app/bootstrap.php';
-
-if (!empty($_SESSION['user_id'])) {
-    redirect(is_admin() ? url('modules/admin/dashboard.php') : url('dashboard.php'));
-}
-
-redirect(url('login.php'));
